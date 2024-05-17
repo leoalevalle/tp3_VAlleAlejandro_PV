15b6938cb4c468b136cbdb7cb626f9cc4354c924
usuario <usuario@DESKTOP-2FH21K9>
2024-05-17 17:22:08 -0300

    Proyecto maven creado.

---
+++ tp3_ValleAlejandro/src/main/java/ar/edu/unju/fi/Tp3ValleAlejandroApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3ValleAlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3ValleAlejandroApplication.class, args);
+	}
+
+}
